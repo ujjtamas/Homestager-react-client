489fd796cbb5a515d5a46b93eb41d880d48f6030
Tamás Ujj <Tamas.Ujj@LEGO.com>
2022-03-10 09:26:20 +0100

    API_URL fix

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-const API_URL = "http://localhost:5005";
+const API_URL = process.env.REACT_APP_API_URL;
 
 const AuthContext = React.createContext();
 
